f4b5b6c5b93f9ab7b837af504a0eba922af5df8b
Johnnboi <email@example.com>
2024-11-12 12:49:16 +0100

    Word Count Working!

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 def main():
-    book_path = "books/frankenstein.txt"
+    book_path = "/root/workspace/github.com/Johnyboi77/bookbot/books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(text)
+    words_counted = count_words(text)
+    print("Number of words:", words_counted)
 
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
-if __name__ == "__main__":  
-
 def count_words (text):
-        word_list = text.split()
-        word_count = 0
-        for word in word_list:
-            word_count += 1
-        return word_count
+    word_list = text.split()
+    word_count = 0
+    for word in word_list:
+        word_count += 1
+    return word_count
 
+if __name__ == "__main__":  
 
     main()
