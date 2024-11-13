ad495f3d740bbd18a64a615ea562779ef51f017c
Johnnboi <email@example.com>
2024-11-13 17:36:24 +0100

    Report working

--- main.py
+++ main.py
@@ -1,32 +1,54 @@
 def main():
     book_path = "/root/workspace/github.com/Johnyboi77/bookbot/books/frankenstein.txt"
     text = get_book_text(book_path)
-    words_counted = count_words(text)
-    character_counted = count_characters(text)
-    print("Number of words:", words_counted)
-    print("character counted", character_counted)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document")
+    print()
+
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
+
+    print("--- End report ---")
+
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+
+def sort_on(d):
+    return d["num"]
+
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
+
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
-def count_words (text):
-    word_list = text.split()
-    word_count = 0
-    for word in word_list:
-        word_count += 1
-    return word_count
 
-def count_characters (text):
-    text = text.lower()
-    character_count = {}
-    for character in text: 
-        if character in character_count: 
-            character_count [character]+= 1
-        else: 
-            character_count [character] = 1
-    return character_count
+main()
 
-if __name__ == "__main__":  
-
-    main()
