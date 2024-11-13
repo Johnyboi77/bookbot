8cffe96ba9ac4661dd3e07213f2618b8ba645c43
Johnnboi <email@example.com>
2024-11-13 16:38:15 +0100

    Count character works

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ def main():
     book_path = "/root/workspace/github.com/Johnyboi77/bookbot/books/frankenstein.txt"
     text = get_book_text(book_path)
     words_counted = count_words(text)
+    character_counted = count_characters(text)
     print("Number of words:", words_counted)
-
+    print("character counted", character_counted)
 
 def get_book_text(path):
     with open(path) as f:
@@ -15,6 +16,16 @@ def count_words (text):
     for word in word_list:
         word_count += 1
     return word_count
+
+def count_characters (text):
+    text = text.lower()
+    character_count = {}
+    for character in text: 
+        if character in character_count: 
+            character_count [character]+= 1
+        else: 
+            character_count [character] = 1
+    return character_count
 
 if __name__ == "__main__":  
 
